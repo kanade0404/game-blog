428a1adee1a453ad8ab516e8da8a665be9a79bf9
kanade <melty0404@gmail.com>
2021-03-13 20:31:48 +0900

    記事一覧のコンポーネントを組み込む

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,41 +1,22 @@
-import React, {FC, useEffect, useState} from 'react';
+import React, {FC, useState} from 'react';
 import 'antd/dist/antd.css';
-import {Typography, Divider} from 'antd';
-import {FolderOutlined} from '@ant-design/icons';
-import {findAll} from '../../driver/ArticleDriver';
 import {Category} from '../../domain/category';
 import {Article} from '../../domain/article';
-import BasePage from '../BasePage';
-
-const {Title, Link} = Typography;
+import {ArticleList} from './components/ArticleList';
+import {useQuery} from './modules/useQuery';
 
 const Home: FC = () => {
   const [articles, setArticles] = useState<Article[]>([]);
   const [categories, setCategories] = useState<Category[]>([]);
-  useEffect(() => {
-    const initArticle = async () => {
-      const articleList = await findAll();
-      const categoryList = articleList.map((article) => ({
-        id: article.category.id,
-        name: article.category.name,
-      }));
-      setArticles(articleList);
-      setCategories(categoryList);
-    };
-    initArticle();
-  }, []);
-  const articleListElement = articles.map((article) => (
-    <div key={article.id} id={`${article.id}`}>
-      <Title level={3}>
-        <Link href={`/article/${article.id}`}>{article.title}</Link>
-      </Title>
-      <FolderOutlined />
-      <span>{article.category.name}</span>
-      <Divider />
-    </div>
-  ));
+  const categoryId = useQuery();
   return (
-    <BasePage pageComponent={articleListElement} categories={categories} />
+    <ArticleList
+      articles={articles}
+      categories={categories}
+      setArticles={setArticles}
+      setCategories={setCategories}
+      categoryId={categoryId}
+    />
   );
 };
 
